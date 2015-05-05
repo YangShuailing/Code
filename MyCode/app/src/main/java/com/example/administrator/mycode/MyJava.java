a2ebd5f6727c8145500ad5fed7c85fe5b4581ebf
YangShuailing <yangsl1008@gmail.com>
2015-05-05 20:43:51 +0800

    LeetCode
    LeetCode  day1

---
+++ MyCode/app/src/main/java/com/example/administrator/mycode/MyJava.java
@@ -0,0 +1,11 @@
+package com.example.administrator.mycode;
+
+/**
+ * Created by Administrator on 2015/4/19 0019.
+ */
+public class MyJava {
+    public static  void main (String[] args){
+
+        System.out.println("Hl");
+    }
+}
